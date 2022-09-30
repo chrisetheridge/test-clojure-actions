e4cf9aa37c5e0fdee77f4e2fe2d63abdae4fb428
Chris Etheridge <chrise@cognician.com>
2022-09-30 15:15:36 +0200

    Add bad file in branch

--- src/foo/core.clj
+++ src/foo/core.clj
@@ -6,13 +6,23 @@
           :number-that-is-unbalanced 5}
          :baz :qux))
 
+(defn foo-bar [x]
+  (do
+ (inc 1)
+ (inc 3)
+ (map inc [1
+
+              2
+
+              3])))
+
 (defn kondo-issues [x y z]
-  (if x
-    true))
+  (if  x
+ true))
 
 (defn add-another-bad-fn  [b a]
   (inc 1)
-  (not (empty? b)))
+(not (empty? b)))
 
-(defn another-bad-fn [xb]
+(defn another-bad-fn-x1 [xb]
   (assoc-in xb [:b] 2))
